419720c10bbe26c966ab0d57e94542e1219f2383
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2026-04-09 07:10:38 +0900

    ヘッターページスクロール

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,2 +1,18 @@
 // modules
 import "./modules/news";
+
+// ヘッダー スクロール制御
+const header = document.querySelector('.js-header');
+const mv = document.querySelector('.mv');
+if (header && mv) {
+  const updateHeader = () => {
+    const mvBottom = mv.getBoundingClientRect().bottom + window.scrollY;
+    if (window.scrollY > mvBottom) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  };
+  window.addEventListener('scroll', updateHeader, { passive: true });
+  updateHeader();
+}
--- src/asset/js/main.js
+++ src/asset/js/main.js
@@ -1,2 +1,17 @@
 (() => {
+  // _dev/js/main.js
+  var header = document.querySelector(".js-header");
+  var mv = document.querySelector(".mv");
+  if (header && mv) {
+    const updateHeader = () => {
+      const mvBottom = mv.getBoundingClientRect().bottom + window.scrollY;
+      if (window.scrollY > mvBottom) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    };
+    window.addEventListener("scroll", updateHeader, { passive: true });
+    updateHeader();
+  }
 })();
